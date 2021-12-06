4a262b438142f09c461037f6ab692f10a3ed775f
Chaeeun Ahn <noteace@naver.com>
2021-12-07 01:46:40 +0900

    로그인, 로그아웃, 회원가입 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('signup/', signup, name='signup'),
+    path('home/signup/', signup, name='signup'),
+    path('home/login/', login, name='login'),
+    path('home/logout/', logout, name='logout'),
+    path('home/', home, name='home'),
+    path('signup2/', signup2, name='signup2'),    
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib import auth
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
@@ -11,8 +12,50 @@ def signup(request):
             user = User.objects.create_user(
                                             username=request.POST['username'],
                                             password=request.POST['password1'],
-                                            email=request.POST['email'],)
+                                           email=request.POST['email'],)
             auth.login(request, user)
             return redirect('/')
         return render(request, 'signup.html')
     return render(request, 'signup.html')
+
+def signup2(request): 
+    if request.method == 'POST': 
+	    if request.POST['password1'] == request.POST['password2']: 
+		    user = User.objects.create_user( 
+			                                username=request.POST['username'], 
+			                                password=request.POST['password1'], 
+			                                email=request.POST['email'],) 
+		    auth.login(request, user) 
+		    return redirect('/') 
+	    return render(request, 'signup.html') 
+    else: 
+	    form = UserCreationForm 
+	    return render(request, 'signup.html', {'form':form})
+
+
+#로그인
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {error: 'username or password is incorrect.'})
+    else:
+        return render(request, 'login.html')
+
+
+#로그아웃
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
+
+#home
+def home(request):
+    return render(request, 'home.html')                        
+
+
+
