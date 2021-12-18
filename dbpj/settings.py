d3efc631e10fafcf0ddd0c11ebb50e8fbccb661a
Chaeeun Ahn <noteace@naver.com>
2021-12-18 20:40:34 +0900

    수정

--- dbpj/settings.py
+++ dbpj/settings.py
@@ -10,10 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-import pymysql
 from pathlib import Path
-pymysql/install_as_MYSQLdb()
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
