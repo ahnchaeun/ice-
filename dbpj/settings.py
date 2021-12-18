bfcd7d0e0bca72ac18a186b562f4db6264f7ac84
Chaeeun Ahn <noteace@naver.com>
2021-12-18 21:50:46 +0900

    배포수정

--- dbpj/settings.py
+++ dbpj/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import pymysql
+pymysql.install_as_MySQLdb()
 import os
 from pathlib import Path
 
