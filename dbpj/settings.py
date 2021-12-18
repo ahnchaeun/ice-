73de19a90f9a99d2b2cdd03f31d6ae3d0403b6de
Chaeeun Ahn <noteace@naver.com>
2021-12-18 20:34:12 +0900

    배포관련오류수정

--- dbpj/settings.py
+++ dbpj/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import pymysql
 from pathlib import Path
+pymysql/install_as_MYSQLdb()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
